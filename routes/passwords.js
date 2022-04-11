8534c29c13a167e0cdb9963ca9eed711877e8bed
Lisa Meyer <lisamermey@gmail.com>
2022-04-11 12:44:01 -0400

    Plan helper functions for passwords route

--- routes/passwords.js
+++ routes/passwords.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
+// HELPER FUNCTIONS
+/**
+ * Edit password information (AJAX)
+ *
+ * Filter by category (AJAX)
+ *
+ * Create new password XXX part of new_password route
+ *
+ * Clipboard API
+ */
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM passwords;`)
