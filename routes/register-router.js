d4bec3df0c4ee062521d9d9648b4803344d1bfbf
Michael Law <mikelaw86@hotmail.com>
2022-04-12 18:41:49 -0400

    Register helper function and start SCSS file

--- routes/register-router.js
+++ routes/register-router.js
@@ -8,22 +8,20 @@ const createUser = (db, userInfo) => {
   const { id, email, password } = userInfo;
 
   if (!email || !password) {
-    return { error: "400 Bad Request - Invalid credentials!", data: null }
+    return { error: "400 Bad Request - Invalid credentials!", data: null };
   }
 
-  const users = Object.values(userDatabase)
-  const userFound = users.find(user => user.email === email)
+  const users = Object.values(userDatabase);
+  const userFound = users.find((user) => user.email === email);
 
   if (userFound) {
-    return { error: "400 Bad Request - User Already Exists!", data: null}
+    return { error: "400 Bad Request - User Already Exists!", data: null };
   }
 
   const newUser = { id, email, password };
   userDatabase[id] = newUser;
   return { error: null, data: newUser };
-
-}
-
+};
 
 // GET route /register
 router.get("/register", (req, res) => {
