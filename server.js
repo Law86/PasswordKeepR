db12a06b4bd9c056fe3d78540daf77509cc54ad7
Lisa Meyer <lisamermey@gmail.com>
2022-04-13 13:35:37 -0400

    Remove extraneous comments, sample routes

--- server.js
+++ server.js
@@ -34,23 +34,13 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const passwordsRoutes = require("./routes/passwords-router");
 const renderRoutes = require("./routes/renderRoutes");
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/passwords", passwordsRoutes(db));
 app.use("/", renderRoutes);
-// Note: mount other resources here, using the same pattern above
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
