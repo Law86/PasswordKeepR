80a1646510313715ce2e75fbf7f1794b4bd6e0d1
Lisa Meyer <lisamermey@gmail.com>
2022-04-13 12:10:59 -0400

    Remove commented out code

--- public/scripts/new_password.js
+++ public/scripts/new_password.js
@@ -1,17 +1,5 @@
-// THIS CODE DID NOT WORK
-
-// (() => {
-  // const slider = document.getElementById('password-range');
-  // const output = document.getElementById('value');
-  // output.innerHTML = slider.value;
-
-  // slider.oninput = function() {
-  //   output.innerHTML = this.value;
-  // }
-// });
 
 $(() => {
-
 
 // prevents cross-site scripting
 const escape = function(str) {
