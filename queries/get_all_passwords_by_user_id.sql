70df81636583503f2d7f0ff1d524d423f98ecc5b
Lisa Meyer <lisamermey@gmail.com>
2022-04-13 13:37:11 -0400

    Queries filter passwords by category and fetch all by user id

---
+++ queries/get_all_passwords_by_user_id.sql
@@ -0,0 +1,8 @@
+SELECT  website,
+        username,
+        password,
+        categories.category as category
+  FROM categories
+  JOIN passwords ON categories.id = category_id
+  WHERE user_id = 1;
+-- TODO -- make user_id a dynamic input
