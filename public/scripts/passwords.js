7a5e4c67eb806a0745afa8deb73862e3c315f08e
Lisa Meyer <lisamermey@gmail.com>
2022-04-13 21:38:27 -0400

    Comment out non-mvp feature

--- public/scripts/passwords.js
+++ public/scripts/passwords.js
@@ -1,21 +1,7 @@
-$(() => {
-  website,
-  username,
-  category,
-  password
+// $(() => {
 
-});
+//   $('#filter-btn').on('click', );
 
-const createPasswordBox = (body) => {
-  return `
-    <article class="password-box">
-      <section>
-        <h3>${body.website}</h3>
-        <h4>${body.username}</h4>
-        <h4>${body.password}</h4>
-        <h4>${body.category}</h4>
-      </section>
-      <input type="button" value="Edit">
-    </article>
-  `
-}
+// });
+
+
