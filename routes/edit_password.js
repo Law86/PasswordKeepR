7a807fc15262446e08f6654c207aaa6b16a862e8
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-14 14:55:15 -0400

    start of backend post to alter db

---
+++ routes/edit_password.js
@@ -0,0 +1,15 @@
+$(()=>{
+  const editPasswordsBtn = [...document.querySelectorAll(".edit-button")];
+  console.log(editPasswordsBtn);
+  editPasswordsBtn.forEach(btn => {
+    const username = btn.dataset.username;
+    btn.addEventListener("click", () => {
+      console.log(username);
+      $(`.edit-form-${username}`).replaceWith(`<form>
+      <input type="password" name="password" placeholder="Password"></input>
+    </form>`);
+    });
+  });
+});
+
+
