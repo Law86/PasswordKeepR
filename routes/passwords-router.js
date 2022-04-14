aafe8f44a1d4c3a479b341dc9c5d12ce6b144b6d
Lisa Meyer <lisamermey@gmail.com>
2022-04-13 21:30:43 -0400

    Clean up code

--- routes/passwords-router.js
+++ routes/passwords-router.js
@@ -3,19 +3,12 @@ const router  = express.Router();
 
 // HELPER FUNCTIONS
 /**
- * Edit password information (AJAX)
+ * Edit password information
  *
  * Filter by category (AJAX) XXX frontend
  *
- * Create new password XXX part of new_password route
- *
  * Clipboard API XXX frontend
  */
-
-
-// GET /passwords
-
-// module.exports = router;
 
 module.exports = (db) => {
   // create - POST
@@ -97,6 +90,52 @@ module.exports = (db) => {
 
 
   // read one - GET /:id
+  // router.get("/", (res, req) => {
+  //   // const { category } = res.body;
+  //   console.log('res.body', res.body);
+
+  //   let category_id = null;
+
+  //   if (category === 'null') {
+  //     category_id = 1;
+  //   } else if (category === 'social') {
+  //     category_id = 2;
+  //   } else if (category === 'entertainment') {
+  //     category_id = 3;
+  //   } else if (category === 'productivity') {
+  //     category_id = 4;
+  //   } else if (category === 'banking') {
+  //     category_id = 5;
+  //   } else if (category === 'health_wellness') {
+  //     category_id = 6;
+  //   } else if (category === 'misc') {
+  //     category_id = 7;
+  //   }
+
+  //   const user_id = 1; // remove when cookie sessions ok
+  //   // TODO uncomment when cookie session implemented
+  //   // const { user_id } = req.session;
+
+  //   db.query(`
+  //   SELECT  website,
+  //           username,
+  //           password,
+  //           categories.category as category
+  //     FROM categories
+  //     JOIN passwords ON categories.id = category_id
+  //     WHERE user_id = $1
+  //     AND category_id = $2;
+  //   `, [user_id, category_id])
+  //   .then(data => {
+  //     const passwords = data.rows;
+  //     return res.render("passwords", { passwords });
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+  // });
 
   // update - PUT
 
