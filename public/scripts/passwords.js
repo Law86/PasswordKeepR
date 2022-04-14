3c0af56fd3cf6cd665d1d94e294bde12d47bdf4b
Lisa Meyer <lisamermey@gmail.com>
2022-04-14 17:26:10 -0400

    Code logic to filter by category

--- public/scripts/passwords.js
+++ public/scripts/passwords.js
@@ -1,7 +1,26 @@
-// $(() => {
+const categories = {
+  social: 'Social',
+  entertainment: 'Entertainment',
+  productivity: 'Productivity',
+  banking: 'Banking',
+  health_wellness: 'Wellness',
+  misc: 'Miscellaneous'
+}
 
-//   $('#filter-btn').on('click', );
+$(() => {
 
-// });
+  $('#category').on('change', function() {
+    filterByTag(categories[this.value]);
+  });
 
+});
+
+const filterByTag = (tag) => {
+  if (!tag) {
+    $('#password-container').find(`article`).show();
+  } else {
+    $('#password-container').find(`article:not(.${tag})`).hide();
+    $('#password-container').find(`article.${tag}`).show();
+  }
+};
 
