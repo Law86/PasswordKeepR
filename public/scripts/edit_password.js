cf93ea78c3cdef97d9bf3cc8a117e18d0618be57
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-14 21:03:54 -0400

    clean code up

--- public/scripts/edit_password.js
+++ public/scripts/edit_password.js
@@ -1,17 +1,14 @@
 $(()=>{
-  const editPasswordsBtn = [...document.querySelectorAll(".edit-button")]
-  console.log(editPasswordsBtn);
+  const editPasswordsBtn = [...document.querySelectorAll(".edit-button")];
   editPasswordsBtn.forEach(btn => {
     const username = btn.dataset.id;
-    console.log(username);
     btn.addEventListener("click", () => {
-      //console.log(id);
       $(`.edit-form-${username}`).replaceWith(`<form method="POST" action="/passwords/edit-passwords" class="edit_form">
       <input type="password" name="password" placeholder="Password"></input>
       <button type="submit">Submit</button>
         </article>
     </form>`);
-    })
-  })
+    });
+  });
 });
 
